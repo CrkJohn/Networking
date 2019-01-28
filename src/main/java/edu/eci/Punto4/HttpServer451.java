525f226d0de548a14c715a1c64897191c1f46bd0
CrkJohn <JhonhRge@gmail.com>
2019-01-28 17:37:31 -0500

    Punto 4 Terminado
    Co-authored-by: CamiloVelandia <jcamilovelandiab@gmail.com>

--- src/main/java/edu/eci/Punto4/HttpServer451.java
+++ src/main/java/edu/eci/Punto4/HttpServer451.java
@@ -1,5 +1,7 @@
 package edu.eci.Punto4;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
 
@@ -12,55 +14,62 @@ public class HttpServer451 {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
         }
-        Socket clientSocket = null;
-        try {
-            System.out.println("Listo para recibir ...");
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
-
-        while(!clientSocket.isClosed()) {
+        while(true){
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
+                int i = inputLine.indexOf('/') + 1;
+                String urlInputLine ="";
                 System.out.println("Received: " + inputLine);
                 if (inputLine.contains("html")) {
-                    String urlInputLine = "";
-                    int i = inputLine.indexOf('/') + 1;
-                    System.out.println(i);
+
                     while (!urlInputLine.endsWith(".html") && i < inputLine.length()) {
                         urlInputLine += (inputLine.charAt(i++));
                     }
                     String urlDirectoryServer = System.getProperty("user.dir") + "\\html\\" + urlInputLine;
-                    System.out.println("Directory server " + urlDirectoryServer);
                     try {
                         BufferedReader readerFile = new BufferedReader(new InputStreamReader(new FileInputStream(urlDirectoryServer), "UTF8"));
                         out.println("HTTP/2.0 200 OK");
                         out.println("Content-Type: text/html");
                         out.println("\r\n");
-                        while(readerFile.ready()){
+                        while (readerFile.ready()) {
                             out.println(readerFile.readLine());
                         }
-                    } catch (FileNotFoundException e) {
-                        System.err.println("Entre : ");
-                        out.println("HTTP/2.0 404 Not found.");
-                        out.println("Content-Type: text/html");
-                        out.println("\r\n");
 
+                    }catch (FileNotFoundException e) {
+                        //out.println("HTTP/2.0 404 Not found.");
+                        //out.println("Content-Type: text/html");
+                        //out.println("\r\n");
                     }
+                }else if(inputLine.contains("jpg")){
+                    while (!urlInputLine.endsWith(".jpg") && i < inputLine.length()) {
+                        urlInputLine += (inputLine.charAt(i++));
+                    }
+                    BufferedImage github = ImageIO.read(new File(System.getProperty("user.dir") + "\\html\\" +urlInputLine));
+                    out.println("HTTP/2.0 200 OK");
+                    out.write("Content-Type: image/webp,*/*");
+                    out.println("\r\n");
+                    ImageIO.write(github, "jpg",clientSocket.getOutputStream());
                 }
                 if (!in.ready()) {
                     break;
                 }
-                out.close();
             }
             in.close();
+            out.close();
+            clientSocket.close();
         }
-        clientSocket.close();
-        serverSocket.close();
+        //serverSocket.close();
+
     }
 
 
